9c215317d51c0fd432892f885bc0eaa728475cf9
toxxxa <toxxxa@gmail.com>
2021-09-20 14:10:43 +0300

    Иницилизируем логгер за пределами хендлера и подключаем его через DI

--- index.php
+++ index.php
@@ -5,21 +5,27 @@ namespace App;
 use App\Controller\PageController;
 use App\Message\PageVisitMessage;
 use App\MessageHandler\PageVisitMessageHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Symfony\Component\Messenger\Handler\HandlersLocator;
 use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
 
 require_once 'vendor/autoload.php';
 
+// Init Logger
+$logger = new Logger('PageVisit');
+$logger->pushHandler(new StreamHandler(__DIR__ . '/logs/page_visit.log', Logger::DEBUG));
+
 // Init Messenger
-$handler = new PageVisitMessageHandler();
+$handler = new PageVisitMessageHandler($logger);
 $bus = new MessageBus([
     new HandleMessageMiddleware(new HandlersLocator([
         PageVisitMessage::class => [$handler],
     ])),
 ]);
 
-$index = new PageController($bus);
+$index = new PageController($logger, $bus);
 
 try {
     echo $index->index();
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Message\PageVisitMessage;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class PageController
 {
+    private LoggerInterface $logger;
+
     private MessageBusInterface $messageBus;
 
-    public function __construct(MessageBusInterface $messageBus)
+    public function __construct(LoggerInterface $logger, MessageBusInterface $messageBus)
     {
+        $this->logger = $logger;
         $this->messageBus = $messageBus;
     }
 
@@ -20,6 +24,8 @@ class PageController
     public function index()
     {
         $this->messageBus->dispatch(new PageVisitMessage('New Message'));
+        $this->logger->debug('Page index completed');
+
         return json_encode('load', JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE);
     }
 }
